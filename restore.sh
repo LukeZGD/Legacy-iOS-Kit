205b17c765df39c269226a5c3e6366988b03924a
LukeeGD <l32luke@gmail.com>
2020-03-11 07:33:23 +0800

    Fix 18.04 libpng12

--- restore.sh
+++ restore.sh
@@ -453,7 +453,7 @@ function Ubuntu1804 {
     ar x libcurl3* data.tar.xz
     tar xf data.tar.xz
     sudo cp usr/lib/$mname-linux-gnu/libcurl.so.4.* /usr/lib/libcurl.so.3
-    curl -L http://mirrors.edge.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1.1_$mname.deb -o libpng12.deb
+    curl -L http://mirrors.edge.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1.1_$mtype.deb -o libpng12.deb
     sudo dpkg -i libpng12.deb
     cd ..
 }
