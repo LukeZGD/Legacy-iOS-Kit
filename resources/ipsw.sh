42bf686698ec63a546d1bcd6c876920bf937546f
LukeZGD <l32luke@gmail.com>
2021-09-29 12:58:29 +0800

    Update ipsw.sh

--- resources/ipsw.sh
+++ resources/ipsw.sh
@@ -70,12 +70,14 @@ IPSW32() {
     ExtraArgs+="-bbupdate"
 
     if [[ ! -e $IPSWCustom.ipsw ]]; then
-        Echo "* By default, memory option is set to Y."
-        Echo "* Make sure that you have at least 8GB of RAM for it to work!"
-        Echo "* If it freezes or fails, this may mean that you do not have enough RAM."
-        Echo "* You may select N if this happens, but make sure that you have enough storage space."
-        read -p "$(Input 'Memory option? (press Enter/Return if unsure) (Y/n):')" JBMemory
-        [[ $JBMemory != 'N' && $JBMemory != 'n' ]] && ExtraArgs+=" -memory"
+        if [[ $platform != "win" ]]; then
+            Echo "* By default, memory option is set to Y."
+            Echo "* Make sure that you have at least 8GB of RAM for it to work!"
+            Echo "* If it freezes or fails, this may mean that you do not have enough RAM."
+            Echo "* You may select N if this happens, but make sure that you have enough storage space."
+            read -p "$(Input 'Memory option? (press Enter/Return if unsure) (Y/n):')" JBMemory
+            [[ $JBMemory != 'N' && $JBMemory != 'n' ]] && ExtraArgs+=" -memory"
+        fi
         Log "Preparing custom IPSW..."
         cd resources
         rm -rf FirmwareBundles
