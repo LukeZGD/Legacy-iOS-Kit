9690f00041e5a525048857848c2b0ff9c232d873
LukeZGD <l32luke@gmail.com>
2021-10-21 18:08:18 +0800

    Update ipsw.sh

--- resources/ipsw.sh
+++ resources/ipsw.sh
@@ -90,3 +90,30 @@ IPSW32() {
     Log "Setting restore IPSW to: $IPSWCustom.ipsw"
     IPSWRestore=$IPSWCustom
 }
+
+IPSW64() {
+    if [[ $IPSWRestore == $IPSWCustom ]]; then
+        Log "Found existing Custom IPSW. Skipping IPSW creation."
+        return
+    fi
+
+    Log "Preparing custom IPSW..."
+    $bspatch $IPSW/Firmware/dfu/$iBSS.im4p $iBSS.im4p resources/patches/$iBSS.patch
+    $bspatch $IPSW/Firmware/dfu/$iBEC.im4p $iBEC.im4p resources/patches/$iBEC.patch
+    if [[ $ProductType == "iPad4"* ]]; then
+        $bspatch $IPSW/Firmware/dfu/$iBSSb.im4p $iBSSb.im4p resources/patches/$iBSSb.patch
+        $bspatch $IPSW/Firmware/dfu/$iBECb.im4p $iBECb.im4p resources/patches/$iBECb.patch
+        mv -f $iBSSb.im4p $iBECb.im4p $IPSW/Firmware/dfu
+    fi
+    mv -f $iBSS.im4p $iBEC.im4p $IPSW/Firmware/dfu
+    cd $IPSW
+    zip -rq0 ../$IPSWCustom.ipsw *
+    cd ..
+    mv $IPSW $IPSWCustom
+    if [[ ! -e $IPSWCustom.ipsw ]]; then
+        Error "Failed to find custom IPSW. Please run the script again"
+    fi
+
+    Log "Setting restore IPSW to: $IPSWCustom.ipsw"
+    IPSWRestore=$IPSWCustom
+}
