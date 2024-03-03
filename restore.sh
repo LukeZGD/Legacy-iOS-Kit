526a4e535ae417a0b371008ccd08deaa28a2827e
LukeZGD <26163116+LukeZGD@users.noreply.github.com>
2024-03-03 20:53:07 +0800

    Fix some commands in erase78

--- restore.sh
+++ restore.sh
@@ -4218,10 +4218,12 @@ device_ramdisk64() {
     fi
     device_sshpass alpine
 
-    print "* Mount filesystems with this command (for newer iOS versions only!):"
+    print "* Mount filesystems with this command (for iOS 11 and newer):"
     print "    mount_filesystems"
-    print "* Mount root filesystem with this command (tested for iOS 10.3.x):"
-    print "    mount_apfs /dev/disk0s1s1 /mnt1"
+    print "* Mount filesystems with this command (for iOS 10.3.x):"
+    print "    mount_apfs /dev/disk0s1s1 /mnt1; mount_apfs /dev/disk0s1s2 /mnt2"
+    print "* Mount filesystems with this command (for iOS 10.2.1 and older):"
+    print "    mount_hfs /dev/disk0s1s1 /mnt1; mount_hfs /dev/disk0s1s2 /mnt2"
 
     menu_ramdisk $build_id
 }
@@ -4715,7 +4717,7 @@ menu_ramdisk() {
                 pause
                 $ssh -p $ssh_port root@127.0.0.1 "mount_hfs /dev/disk0s1s1 /mnt1; mount_hfs /dev/disk0s1s2 /mnt2; cp /com.apple.springboard.plist /mnt1/"
                 $ssh -p $ssh_port root@127.0.0.1 "cd /mnt2/mobile/Library/Preferences; mv com.apple.springboard.plist com.apple.springboard.plist.bak; ln -s /com.apple.springboard.plist ./com.apple.springboard.plist"
-                $ssh -p $ssh_port root@127.0.0.1 "sync cd / umount /mnt2; umount /mnt1; sync; reboot"
+                $ssh -p $ssh_port root@127.0.0.1 "sync; cd /; umount /mnt2; umount /mnt1; sync; reboot"
                 log "Done, your device should reboot now"
                 print "* If your device is on iOS 7, make sure to boot an iOS 8 ramdisk to fix booting."
                 print "* Proceed to trigger a restore by entering wrong passwords 10 times."
